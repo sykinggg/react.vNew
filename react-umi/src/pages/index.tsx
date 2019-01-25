29b5edbf9c99aa82b89b7bcbf883bcfb599539d3
sy <yisui@skspruce.net>
2019-01-25 17:31:21 +0800

    优化

--- react-umi/src/pages/index.tsx
+++ react-umi/src/pages/index.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import styles from './index.less';
+import './index.css';
 import { Layout, Menu, Breadcrumb, Icon, } from 'antd';
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
 
 export default function () {
     return (
-        <Layout>
-            <Header className="header">
+        <Layout className="text-left">
+            <Header className="header" style={{padding: '0px'}}>
                 <div className="logo"></div>
                 <Menu
                     theme="dark"
                     mode="horizontal"
                     defaultSelectedKeys={['2']}
-                    style={{ lineHeight: '64px' }}
-                >
-                    <Menu.Item key="1">nav 1</Menu.Item>
-                    <Menu.Item key="2">nav 2</Menu.Item>
-                    <Menu.Item key="3">nav 3</Menu.Item>
+                    // tslint:disable-next-line:jsx-alignment
+                    style={{ lineHeight: '64px', textAlign: 'left' }}>
+                    <Menu.Item key="test"></Menu.Item>
                 </Menu>
             </Header>
             <Layout></Layout>
---
+++ react-umi/src/pages/test.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+import styles from './test.css';
+
+export default function () {
+    return (
+        <div className={styles.normal}>
+            <h1>Page test</h1>
+        </div>
+    );
+}
