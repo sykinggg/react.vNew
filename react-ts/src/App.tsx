1043016cd18d7adf381162eba5843532f06c6034
sy <yisui@skspruce.net>
2019-03-13 15:36:34 +0800

    同步

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react';
 import './App.scss';
 
+import { Button } from 'antd';
 import logo from './logo.svg';
 
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import About from './view/about';
+import Home from './view/home';
+
 class App extends React.Component {
+    constructor(props: Readonly<{}>) {
+        super(props);
+    }
     public render() {
         return (
             <div className="App">
@@ -13,7 +21,24 @@ class App extends React.Component {
                 </header>
                 <p className="App-intro">
                     To get started, edit <code>src/App.tsx</code> and save to reload.
+                    <Button type="primary">Button</Button>
                 </p>
+                <Router>
+                    <div>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <Link to="home">home</Link>
+                                </li>
+                                <li>
+                                    <Link to="about">about</Link>
+                                </li>
+                            </ul>
+                        </nav>
+                        <Route path="/home" component={Home} />
+                        <Route path="/about" component={About} />
+                    </div>
+                </Router>
             </div>
         );
     }
---
+++ react-ts/src/view/about.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+class About extends React.Component {
+    public render() {
+        return (
+            <div>about</div>
+        )
+    }
+}
+
+export default About;
