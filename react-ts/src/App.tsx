1a5d1ec27e8fdf3e02c21c44c7d25a3d113279e9
sy <yisui@skspruce.net>
2019-03-13 10:36:55 +0800

    before eject

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,23 +1,22 @@
-import Button from 'antd/lib/button';
 import * as React from 'react';
 import './App.css';
+
 import logo from './logo.svg';
 
 class App extends React.Component {
-    public render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <Button type="primary">Button</Button>
-            </div>
-        );
-    }
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
 }
 
 export default App;
